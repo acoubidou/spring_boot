fe2144946b44f0c696fcce87fb0f07a3a5298455
Sami <sami;achbani@gmail.com>
2025-04-25 13:49:00 +0200

    New dockerfile

--- src/test/java/com/sami/springboot/ApplicationTests.java
+++ src/test/java/com/sami/springboot/ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = com.sami.springboot.Application.class)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
